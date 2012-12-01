8aa9acc244ef9c1798dfc1371d2d74fab93b8721
Anthony Chan <anthonyhchan@gmail.com>
2012-12-01 14:41:39 +0000

    Finsh user actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/signup', to: 'users#new'
   match '/about',   to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
